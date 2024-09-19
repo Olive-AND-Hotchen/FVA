27a3d0eb3d8342a622f7ae0dbf1200e9d55bd6b2
Michael Drennan <michael.drennan@and.digital>
2024-09-19 16:18:14 +0100

    Made location controller

---
+++ server/controllers/LocationController.cs
@@ -0,0 +1,75 @@
+using FVA.Database;
+using FVA.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LocationController(OdinDatabaseContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<Location>> GetLocations()
+    {
+        return await context.Locations.ToListAsync();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<Location>> GetLocation(int id)
+    {
+        var location = await context.Locations.FindAsync(id);
+
+        if (location == null)
+            return NotFound();
+        return location;
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> PutLocation(int id, Location location)
+    {
+        if (id != location.Id) return BadRequest();
+
+        context.Entry(location).State = EntityState.Modified;
+
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!LocationExists(id)) return NotFound();
+
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Location>> PostLocation(Location location)
+    {
+        context.Locations.Add(location);
+        await context.SaveChangesAsync();
+
+        return CreatedAtAction("GetLocation", new { id = location.Id }, location);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteLocation(int id)
+    {
+        var location = await context.Locations.FindAsync(id);
+        if (location == null) return NotFound();
+
+        context.Locations.Remove(location);
+
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool LocationExists(int id)
+    {
+        return context.Locations.Any(e => e.Id == id);
+    }
+}
