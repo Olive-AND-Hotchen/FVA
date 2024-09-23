f8d2c53dfe4e27d035562e42661e8f90a8300142
shaun-and <shaun.fraser@and.digital>
2024-09-23 09:58:45 +0100

    Added all references for Mudblazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,13 @@
 using Client;
 using Client.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(_ =>
     new HttpClient
